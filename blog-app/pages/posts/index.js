536d9a084786487b1e613510c830263aad50e9ea
Anurag Asthana <a.asthana90@gmail.com>
2023-10-29 18:23:50 +0530

    final blog application

--- blog-app/pages/posts/index.js
+++ blog-app/pages/posts/index.js
@@ -1,5 +1,25 @@
+import AllPosts from "../../components/posts/all-posts";
+
 const AllPostPage = () => {
 
+    const dummy = [{
+        slug:'getting-started-with-next-js',
+        title:'Getting Started with Next JS',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJS is a react framework for production',
+        date:'2023-02-10'
+      },
+      {
+        slug:'getting-started-with-next-js1',
+        title:'Getting Started with Next JS',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJS is a react framework for production',
+        date:'2023-03-10'
+      }];
+
+    return (
+        <AllPosts posts={dummy}></AllPosts>
+    )
 }
 
 export default AllPostPage;
